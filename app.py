3940a27d087a9a345a7542cbe2eab13937e8fc79
Engjell <Engjot22@gmail.com>
2022-10-16 23:33:21 -0400

    Updated python code to make buttons work.

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('kop.html')
+    return render_template('KOP.html')
 
+@app.route('/Welcome', methods=['GET', 'POST'])
+def welcome():
+    return render_template('Welcome.html')
+
+
+@app.route('/SignUp', methods=['GET', 'POST'])
+def signup():
+    return render_template('SignUp.html')
+
+
+
+@app.route('/Fpass', methods=['GET', 'POST'])
+def password():
+    return render_template('Fpass.HTML')
+
+@app.route('/TOS', methods=['GET', 'POST'])
+def TermsAndCoditions():
+    return render_template('TOS.html')
 
 app.run()
